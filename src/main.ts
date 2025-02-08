1ad477702bb6d9a4f57e0864d3f49a203c92d335
Kai Itakura <itakai199969@gmail.com>
2025-02-08 11:48:29 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // logger
+  app.useLogger(app.get(Logger));
+
+  // validation
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  await app.listen(app.get(ConfigService).getOrThrow('PORT'));
+}
+bootstrap();
